eed331087e74bcf659f2bc372caa77608c06cbaf
zach <zekariyasamdu24@gmail.com>
2026-01-10 17:20:30 -0600

    merge_sort

---
+++ src/algorithms/sorting/merge_sort.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void merge_sort(int arr[], int left, int right) {
+    if (left >= right)
+        return;
+
+    int mid = left + (right - left) / 2;
+
+    merge_sort(arr, left, mid);
+    merge_sort(arr, mid + 1, right);
+
+    merge(arr, left, mid, right);
+}
+
+int main() {
+    int arr[] = {7, 4, 9, 1, 5, 8, 2};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Before sort:\n";
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+
+    merge_sort(arr, 0, size - 1);
+
+    cout << "\n\nAfter sort:\n";
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
